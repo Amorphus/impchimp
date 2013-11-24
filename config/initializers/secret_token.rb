7af901bc2415172a30f2937d90638d8332998872
Grant <grant@grant-VirtualBox.(none)>
2013-11-24 13:11:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Impchimp::Application.config.secret_key_base = '4b2120c18587fe3f5971c4a2a232f8a21b945584d37f97a82e6f3a1eaf8868e8a8c8fc9c51867391ca7433bd149e668cf7247d738be9e423fa4c85e35f5deef4'
